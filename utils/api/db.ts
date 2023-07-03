6c25d513d0640e7c60b634d77dd01f941a59247a
he.wenyao <he.wenyao@outlook.com>
2023-07-03 17:08:01 +0800

    fix

--- utils/api/db.ts
+++ utils/api/db.ts
@@ -1,8 +1,10 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
+
 const pool: mysql.Pool = mysql.createPool({
-  host: 'xxxxxxxxxxxxxxx',
-  user: 'root',
-  password: 'xxxxxxxxxxxxxx',
-  database: 'xxxxxxxxxxxxxxx'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
-export default pool
+
+export default pool;
