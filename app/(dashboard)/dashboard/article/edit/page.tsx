3dde830df57fd9ef6d31711376cdf256e49f82d2
he.wenyao <he.wenyao@qq.com>
2023-07-09 20:26:33 +0800

    fix

--- app/(dashboard)/dashboard/article/edit/page.tsx
+++ app/(dashboard)/dashboard/article/edit/page.tsx
@@ -5,7 +5,7 @@ export default function ArticleEdit() {
     <div className="flex justify-between w-full h-full">
       <div className="w-32">文章列表</div>
       <div className="flex-1 h-full">
-        <MarkdownEditor className={"bg-dark text-white"} />
+        <MarkdownEditor  />
       </div>
     </div>
   );
